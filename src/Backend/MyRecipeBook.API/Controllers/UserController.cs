001141d8a2bf74856d7b5de070b133f4cd423dfa
Lenilson Soares <lenilsonp40@gmail.com>
2024-07-31 16:51:52 -0300

    estruct project

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.User.Register;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Register(RequestRegisterUserJson request) 
+        {
+            var userCase =  new RegisterUserUseCase();
+
+            var result = userCase.Execute(request);
+
+            return Created(string.Empty, result);
+        }
+    }
+}
---
+++ src/Backend/MyRecipeBook.Application/UseCases/User/Register/RegisterUserUseCase.cs
@@ -0,0 +1,21 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Application.UseCases.User.Register
+{
+    public class RegisterUserUseCase
+    {
+        public ResponseRegisteredUserJson Execute(RequestRegisterUserJson request)
+        {
+            return new ResponseRegisteredUserJson
+            {
+                Name = request.Name,
+            };
+        }
+    }
+}
